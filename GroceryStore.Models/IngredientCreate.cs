608af60c9b0111aee55bbb65da63bce9e0e53da3
nmalcock <nmalcock97@gmail.com>
2021-07-22 11:23:55 -0400

    Added and edited Ingredient Create and Ingredient Services Files

---
+++ GroceryStore.Models/IngredientCreate.cs
@@ -0,0 +1,21 @@
+﻿using GroceryStore.Date;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryStore.Models
+{
+    public class IngredientCreate
+    {
+        [Required]
+        public string IngredientName { get; set; }
+        [Required]
+        [ForeignKey(nameof(Recipe))]
+        public int RecipeId{ get; set; }
+        public virtual Recipe Recipe { get; set; }
+    }
+}
