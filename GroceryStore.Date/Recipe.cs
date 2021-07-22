12cd98498b64b2550fd7c06f6ef2096d97af9612
Alexandria Hardin <aphardin101@gmail.com>
2021-07-22 10:12:04 -0500

    added recipe class

---
+++ GroceryStore.Date/Recipe.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryStore.Date
+{
+    public enum DietType
+    {
+        Vegan,
+        Vegetarian,
+        Pescatarian, 
+        Gluten_Free,
+        Nut_Free
+    }
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Minutes { get; set; }
+        public DietType TypeOfDiet { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
