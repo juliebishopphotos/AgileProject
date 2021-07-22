79caa59e0af2ea6267d9aea89f5f4b12fa4ba453
julie <juliebishopphotos@gmail.com>
2021-07-22 11:24:35 -0400

    added grocery list data

---
+++ GroceryStore.Date/GroceryList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryStore.Date
+{
+    public class GroceryList
+    {
+        [Key]
+        public int GroceryListId { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Recipe))]
+        public int RecipeId { get; set; }
+        public virtual Recipe Recipe { get; set; }
+
+        [Required]
+        public Guid UserId { get; set; }
+
+    }
+}
