799d5008799953634db6deb7700da5ef78184c38
julie <juliebishopphotos@gmail.com>
2021-07-22 11:10:15 -0400

    added ingredient class

--- GroceryStore.Date/Ingredient.cs
+++ GroceryStore.Date/Ingredient.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GroceryStore.Date
 {
-    class Ingredient
+    public class Ingredient
     {
+        [Key]
+        public int IngredientId { get; set; }
+
+        [Required]
+        public string IngredientName { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Recipe))]
+        public int RecipeId { get; set; }
+        public virtual Recipe Recipe { get; set; }
     }
 }
